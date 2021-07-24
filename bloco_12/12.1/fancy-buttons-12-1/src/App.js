801ab28b241c7dc08a960f52e7e575aafe60c99f
Camila Dornas <camiladornas@gmail.com>
2021-07-24 19:01:27 -0300

    Atualiza exercicio butoes

--- bloco_12/12.1/fancy-buttons-12-1/src/App.js
+++ bloco_12/12.1/fancy-buttons-12-1/src/App.js
@@ -1,24 +1,31 @@
 import React from 'react';
 
-function handleCLick() {
-  console.log('Click');
-}
-
-function handleCLick2() {
-  console.log('Click 2');
-}
-
-function handleCLick3() {
-  console.log('Click 3');
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleCLick = this.handleCLick.bind(this);
+    this.handleCLick2 = this.handleCLick2.bind(this);
+    this.handleCLick3 = this.handleCLick3.bind(this);
+  }
+
+  handleCLick() {
+    console.log('Click');
+  }
+
+  handleCLick2() {
+    console.log('Click 2');
+  }
+
+  handleCLick3() {
+    console.log('Click 3');
+  }
+
   render() {
     return(
       <div>
-        <button onClick={handleCLick}>Click!</button>
-        <button onClick={handleCLick2}>Click2!</button>
-        <button onClick={handleCLick3}>Click3!</button>
+        <button onClick={this.handleCLick}>Click!</button>
+        <button onClick={this.handleCLick2}>Click2!</button>
+        <button onClick={this.handleCLick3}>Click3!</button>
       </div>
     )
   }
