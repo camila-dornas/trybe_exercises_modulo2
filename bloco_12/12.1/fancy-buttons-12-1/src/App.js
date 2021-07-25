41e08554eaa16189aceda7628762343a3cdf1bbb
Camila Dornas <camiladornas@gmail.com>
2021-07-25 09:40:43 -0300

    Add contagem aos botoes

--- bloco_12/12.1/fancy-buttons-12-1/src/App.js
+++ bloco_12/12.1/fancy-buttons-12-1/src/App.js
@@ -3,6 +3,13 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      botao1: 0,
+      botao2: 0,
+      botao3: 0,
+    }
+
     this.handleCLick = this.handleCLick.bind(this);
     this.handleCLick2 = this.handleCLick2.bind(this);
     this.handleCLick3 = this.handleCLick3.bind(this);
@@ -10,22 +17,31 @@ class App extends React.Component {
 
   handleCLick() {
     console.log('Click');
+    this.setState((estadoAnterior, _props) => ({
+      botao1: estadoAnterior.botao1 + 1
+    }))
   }
 
   handleCLick2() {
     console.log('Click 2');
+    this.setState((estadoAnterior, _props) => ({
+      botao2: estadoAnterior.botao2 + 1
+    }))
   }
 
   handleCLick3() {
     console.log('Click 3');
+    this.setState((estadoAnterior, _props) => ({
+      botao3: estadoAnterior.botao3 + 1
+    }))
   }
 
   render() {
     return(
       <div>
-        <button onClick={this.handleCLick}>Click!</button>
-        <button onClick={this.handleCLick2}>Click2!</button>
-        <button onClick={this.handleCLick3}>Click3!</button>
+        <button key='botao1' onClick={this.handleCLick}>{this.state.botao1}</button>
+        <button key='botao2' onClick={this.handleCLick2}>{this.state.botao2}</button>
+        <button key='botao3' onClick={this.handleCLick3}>{this.state.botao3}</button>
       </div>
     )
   }
